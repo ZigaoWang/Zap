33821da313c1a71488793db542267e7639a04f4d
Zigao Wang <zigao.wang24@gmail.com>
2024-09-18 21:13:56 +0800

    feat: Initial commit

---
+++ Zap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zap
+//
+//  Created by Zigao Wang on 9/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zap/ZapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZapApp.swift
+//  Zap
+//
+//  Created by Zigao Wang on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ZapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
